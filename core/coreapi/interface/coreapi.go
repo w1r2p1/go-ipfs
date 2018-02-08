de1671649aca01e68cd08661ee4350e21633edfb
Łukasz Magiera <magik6k@gmail.com>
2018-02-08 17:39:05 +0100

    coreapi: separate path into two types
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/interface/coreapi.go
+++ core/coreapi/interface/coreapi.go
@@ -5,8 +5,6 @@ package iface
 import (
 	"context"
 
-	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
-
 	ipld "gx/ipfs/QmWi2BYBL5gJ3CiAiQchg6rn1A8iBsrWy51EYxvHVjFvLb/go-ipld-format"
 	cid "gx/ipfs/QmapdYm1b22Frv3k17fqrBYTFRxwiaVJkB299Mfn33edeB/go-cid"
 )
@@ -35,19 +33,15 @@ type CoreAPI interface {
 	Object() ObjectAPI
 
 	// ResolvePath resolves the path using Unixfs resolver
-	ResolvePath(context.Context, Path) (Path, error)
+	ResolvePath(context.Context, Path) (ResolvedPath, error)
 
 	// ResolveNode resolves the path (if not resolved already) using Unixfs
 	// resolver, gets and returns the resolved Node
 	ResolveNode(context.Context, Path) (ipld.Node, error)
 
 	// ParsePath parses string path to a Path
-	ParsePath(context.Context, string, ...options.ParsePathOption) (Path, error)
-
-	// WithResolve is an option for ParsePath which when set to true tells
-	// ParsePath to also resolve the path
-	WithResolve(bool) options.ParsePathOption
+	ParsePath(context.Context, string) (Path, error)
 
 	// ParseCid creates new path from the provided CID
-	ParseCid(*cid.Cid) Path
+	ParseCid(*cid.Cid) ResolvedPath
 }
